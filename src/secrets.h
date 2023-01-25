9361d052c3cf28a38ec9df873dd33d9a90c256fc
semuadmin <28569967+semuadmin@users.noreply.github.com>
2023-01-25 20:44:07 +0000

    init commit

---
+++ src/secrets.h
@@ -0,0 +1,13 @@
+/*
+Current and Next SPARTN keys
+*/
+
+const uint8_t currentKeyLengthBytes =   16; 
+const char currentDynamicKey[] =        "<ADD YOUR L-Band or L-Band + IP DYNAMIC KEY HERE>";
+const uint16_t currentKeyGPSWeek =      2192; // Update this when you add new keys
+const uint32_t currentKeyGPSToW =       518400;
+
+const uint8_t nextKeyLengthBytes =      16; 
+const char nextDynamicKey[] =           "<ADD YOUR L-Band or L-Band + IP DYNAMIC KEY HERE>";
+const uint16_t nextKeyGPSWeek =         2196; // Update this when you add new keys
+const uint32_t nextKeyGPSToW =          518400;
